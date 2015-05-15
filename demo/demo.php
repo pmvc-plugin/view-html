8164df3e345ed9a0f7015ef7b8d6090b64a4ef00
Hill <hill@kimo.com>
2015-05-15 21:30:20 +0400

    update demo

--- demo/demo.php
+++ demo/demo.php
@@ -1,4 +1,9 @@
 <?php
-
-
+include_once('vendor/pmvc/pmvc/include_plug.php');
+PMVC\setPlugInFolder('vendor/pmvc-plugin/');
+$html = PMVC\plug('view-html');
+$html->folder='vendor/pmvc-theme/hello-theme';
+$html->path='hello';
+$html->set('text','hello world');
+echo $html->process();
 
